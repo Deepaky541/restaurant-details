53db105cd5c0ad1647e03f7c7c63306b2527cca0
Deepak yadav <deepaky541.dy@gmail.com>
2022-06-17 00:45:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import { RestaurantDetails } from './components/RestaurantDetails';
 import data from "./db.json"
 
+
 function App() {
+  const [info, setinfo] = useState(data);
+  const [btn, setbtn] = useState(5)
+
   return (
     <div className="App">
-      {data.details.map((el)=>(<RestaurantDetails data={el} />))}
+      {info.details.map((el) => {
+        if (btn === 5) {
+          return <RestaurantDetails data={el} />;
+        } else if (btn === 4 && el.ratings >= 4) {
+          return <RestaurantDetails data={el} />;
+        } else if (btn === 3 && el.ratings >= 3) {
+          return <RestaurantDetails data={el} />;
+        } else if (btn === 2 && el.ratings >= 2) {
+          return <RestaurantDetails data={el} />;
+        } else if (btn === 1 && el.ratings >= 1) {
+          return <RestaurantDetails data={el} />;
+        }
+       
+})}
+      <button onClick={()=>{setbtn(1)}}>1 star and above</button>
+      <button onClick={()=>{setbtn(2)}}>2 star and above</button>
+      <button onClick={()=>{setbtn(3)}}>3 star and above</button>
+      <button onClick={()=>{setbtn(4)}}>4 star and above</button>
+      <button onClick={()=>{setbtn(5)}}>show all</button>
     </div>
   );
 }
--- src/components/RestaurantDetails.jsx
+++ src/components/RestaurantDetails.jsx
@@ -1,33 +1,63 @@
 import React from 'react'
 import "./Restaurant.css"
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 export const RestaurantDetails = ({data}) => {
-  return (
-    <div className="card">
-      <div className='image'>
-        <img src={data.url} alt="" />
-      </div>
-      <div className='title'>
-        <h1>{data.title}</h1>
-        <p>{data.category}</p>
-        <p>Cost Rs. {data.cost} for one</p>
-        <div className='min'>
-         <p>Min Rs{data.min}</p> 
-         <div className='list'>
-          <ul>
-            <li><p>Up to {data.time} min</p> </li>
-          </ul>
+  const [pay, setpayment] = useState("");
+ useEffect(() => {
+     if (
+       data.payments.card === true &&
+       data.payments.cash === true &&
+       data.payments.upi === true
+     ) {
+       setpayment("Accepts all types of payments")
+     } else if (data.payments.card === true && data.payments.cash === true) {
+       setpayment("Accepts card and cash payments");
+     } else if (data.payments.cash === true && data.payments.upi === true) {
+       setpayment("Accepts cash and upi payments");
+     } else if (data.payments.card === true && data.payments.upi === true) {
+       setpayment("Accepts card and upi payments");
+     } else if (data.payments.card === true) {
+       setpayment("Accepts card payments only");
+     } else if (data.payments.cash === true) {
+       setpayment("Accepts cash payments only");
+     } else if (data.payments.upi === true) {
+       setpayment("Accepts upi payments only");
+     }
+ }, [])
+ 
+  
+  
+ 
+  
+    return (
+        <div className="card">
+          <div className="image">
+            <img src={data.url} alt="" />
+          </div>
+          <div className="title">
+            <h1>{data.title}</h1>
+            <p>{data.category}</p>
+            <p>Cost Rs. {data.cost} for one</p>
+            <div className="min">
+              <p>Min Rs{data.min}</p>
+              <div className="list">
+                <ul>
+                  <li>
+                    <p>Up to {data.time} min</p>{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <p>{pay}</p>
+          </div>
+          <div className="section3">
+            <h3>{data.ratings}</h3>
+            <p>{data.votes} votes</p>
+            <p>{data.reviews} reviews</p>
           </div>
         </div>
-        <p>{data.payments}</p>
-      </div>
-      <div className='section3'>
-        <h3 >{data.ratings}</h3>
-        <p>{data.votes} votes</p>
-        <p>{data.reviews} reviews</p>
-      </div>
-     
-    </div> 
-  );
+    );
 }
